fc11d282c00f4a8b844b6856b09834a38ce2285d
Jan Kříž <jan.kriz@mediafactory.cz>
2023-12-08 18:54:32 +0100

    playing with tailwind syntax, adding card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Nav from "@/components/Nav";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Nav />
+      <Card className="bg-white">
+        <p>This is inside the card component.</p>
+      </Card>
     </main>
   );
 }
